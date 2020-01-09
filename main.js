e16b30537be6fdb62bb0639fa391662fc3cd6908
Jonathan Brierre <github email address>
2020-01-09 18:03:45 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeButton = document.querySelectorAll('.like')
 
+let errorDiv = document.querySelector('#modal')
+errorDiv.classList.add('hidden')
 
+likeButton.forEach(button => {
+  button.addEventListener('click', (e)=>{
+  let heart = button.querySelector('span')
+  mimicServerCall()
+    .then(resp => {
+      if(errorDiv.className !== 'hidden'){
+        errorDiv.className = 'hidden'
+      }
+      if(heart.className === 'like-glyph activated-heart'){
+        heart.textContent = EMPTY_HEART
+        heart.classList.remove('activated-heart')
+      }else{
+        heart.textContent = FULL_HEART
+        heart.classList.add('activated-heart')
+      }
+    })
+    .catch(error => {
+      errorDiv.classList.remove('hidden');
+      errorDiv.textContent = `${error}`
+      window.setTimeout((e)=>{
+        errorDiv.classList.add('hidden')
+      }, 3000)
+    })
+  })
+})
 
 
 //------------------------------------------------------------------------------
